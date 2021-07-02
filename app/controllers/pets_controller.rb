f97ba42f47ad46480871f09f00df9de7f967b32b
Bill <billfeng2002@gmail.com>
2021-07-01 17:12:45 -0700

    commit tree a26cc7029cbd36ea1df104e14e3d8cade2f536c5

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,21 +6,41 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners=Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    @pet = Pet.create(params[:pet])
+    if params[:owner]["name"] != ""
+      owner=Owner.create(name: params["owner"]["name"])
+      @pet.update(owner_id: owner.id)
+    else
+      @pet.update(owner_id: params[:owner][:owner_id])
+    end
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get "/pets/:id/edit" do
+    @pet = Pet.find(params[:id])
+    @owners=Owner.all
+    erb :'/pets/edit'
+  end
+
+  get "/pets/:id" do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
-
+  patch "/pets/:id" do
+    @pet = Pet.find(params[:id])
+    @pet.update(params[:pet])
+    if params[:owner]["name"] != ""
+      owner=Owner.create(name: params["owner"]["name"])
+      @pet.update(owner_id: owner.id)
+    else
+      @pet.update(owner_id: params[:owner][:owner_id])
+    end
     redirect to "pets/#{@pet.id}"
   end
 end
